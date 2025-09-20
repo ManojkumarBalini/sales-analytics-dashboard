662dc2725012c17c1bcc05c4d5c81c10e2fda15c
ManojkumarBalini <157731041+ManojkumarBalini@users.noreply.github.com>
2025-09-20 22:19:38 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,6 +10,8 @@ const connectDB = async () => {
     const conn = await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
